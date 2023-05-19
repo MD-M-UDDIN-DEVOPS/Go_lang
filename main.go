1736224ec32ce233812a6e39f4e4ba1dc6fd13f3
minhaj <minhajuddin2008@gmail.com>
2023-05-18 21:47:31 -0400

    commit

--- main.go
+++ main.go
@@ -11,3 +11,15 @@ func main() {
 	fmt.Println(minhaj)
 
 }
+
+package main
+
+import "fmt"
+
+func main() {
+        var a, b bool = true, false
+        fmt.Println(a && b)
+        fmt.Println(a || b)
+}
+
+
