6f39095feec8b8753dcf3462f052eb392f8f80cd
minhaj <minhajuddin2008@gmail.com>
2023-05-24 21:02:50 -0400

    commit

---
+++ if_else_statment.go
@@ -0,0 +1,34 @@
+// with if only
+package main
+
+import "fmt"
+
+func main() {
+	// a program to determine positive / negative / zero
+	number := 10
+	if number > 0 {
+		fmt.Printf("Positive\n")
+	}
+	if number < 0 {
+		fmt.Printf("Negative\n")
+	}
+	if number == 0 {
+		fmt.Printf("Zero\n")
+	}
+}
+
+ // with if, else if
+ package main
+
+ import "fmt"
+ func main(){
+   // a program to determine positive / negative / zero
+   number := 10
+   if number > 0 {
+	 fmt.Printf("Positive\n")
+   }	else if number < 0 {
+	 fmt.Printf("Negative\n")
+   } else if number == 0 {
+	 fmt.Printf("Zero\n")
+   }
+ }
